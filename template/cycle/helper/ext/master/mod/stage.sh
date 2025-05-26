ebaa69fcdb6119a50dfa86d3cf8a330b9c181335
samwhelp <samwhelp@users.noreply.github.com>
2025-05-26 16:31:31 +0800

    fix

--- template/cycle/helper/ext/master/mod/stage.sh
+++ template/cycle/helper/ext/master/mod/stage.sh
@@ -10,6 +10,8 @@ mod_stage_rundown_process_all () {
 
 	mod_stage_rundown_process_factory_install
 
+	mod_stage_rundown_process_basic_install
+
 	mod_stage_rundown_process_skeleton_middle
 
 	mod_stage_rundown_process_master_install
@@ -17,7 +19,6 @@ mod_stage_rundown_process_all () {
 	mod_stage_rundown_process_skeleton_tail
 
 }
-
 
 
 mod_stage_rundown_process_factory_install () {
@@ -46,6 +47,27 @@ mod_stage_rundown_process_master_install () {
 	local module_root_dir_path="${REF_MASTER_STAGE_MODULE_DIR_PATH}"
 
 	local rundown_file_path="${REF_MASTER_STAGE_RUNDOWN_DIR_PATH}/master-install.txt"
+
+
+	util_error_echo
+	util_error_echo "##"
+	util_error_echo "## ## Rundown"
+	util_error_echo "##"
+	util_error_echo
+	util_error_echo "[Rundown]: ${rundown_file_path}"
+	util_error_echo
+
+
+	util_stage_rundown_process "${rundown_file_path}" "${module_root_dir_path}"
+
+}
+
+
+mod_stage_rundown_process_basic_install () {
+
+	local module_root_dir_path="${REF_MASTER_STAGE_MODULE_DIR_PATH}"
+
+	local rundown_file_path="${REF_MASTER_STAGE_RUNDOWN_DIR_PATH}/basic-install.txt"
 
 
 	util_error_echo
