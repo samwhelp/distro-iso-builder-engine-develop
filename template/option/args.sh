ee62a4f72889d949bbfdef2f63af30d5db5a2e35
samwhelp <samwhelp@users.noreply.github.com>
2025-05-27 20:33:00 +0800

    fix

---
+++ template/option/args.sh
@@ -0,0 +1,17 @@
+
+
+##
+## ## REF_BUILD_SUITE
+##
+
+REF_BUILD_SUITE="plucky"
+
+
+
+
+##
+## ## REF_PACKAGE_REPO_URL
+##
+
+#REF_PACKAGE_REPO_URL="http://archive.ubuntu.com/ubuntu/"
+REF_PACKAGE_REPO_URL="http://tw.archive.ubuntu.com/ubuntu/"
