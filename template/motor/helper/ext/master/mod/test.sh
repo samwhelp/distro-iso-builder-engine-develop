867e26b4a51902ecde166f4d143fe5a915833dd1
samwhelp <samwhelp@users.noreply.github.com>
2025-05-27 16:31:02 +0800

    fix

--- template/motor/helper/ext/master/mod/test.sh
+++ template/motor/helper/ext/master/mod/test.sh
@@ -15,7 +15,11 @@ mod_test () {
 
 	#sys_tmp_dir_create
 
-	sys_distro_base_system_create
+	#sys_distro_base_system_create
+
+	#sys_distro_mount_for_chroot
+
+	#sys_distro_unmount_for_chroot
 
 
 }
--- template/motor/helper/ext/master/sys/chroot.sh
+++ template/motor/helper/ext/master/sys/chroot.sh
@@ -23,12 +23,32 @@ sys_chroot_run () {
 
 sys_distro_mount_for_chroot () {
 
+	##
+	## https://github.com/mvallim/live-custom-ubuntu-from-scratch/blob/master/scripts/build.sh#L46-L52
+	##
+
+	sudo mount --bind /dev "${REF_DISTRO_IMG_DIR_PATH}"/dev
+	sudo mount --bind /run "${REF_DISTRO_IMG_DIR_PATH}"/run
+
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" mount none -t proc /proc
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" mount none -t sysfs /sys
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" mount none -t devpts /dev/pts
 
 	return 0
 }
 
 sys_distro_unmount_for_chroot () {
 
+	##
+	## https://github.com/mvallim/live-custom-ubuntu-from-scratch/blob/master/scripts/build.sh#L54-L60
+	##
+
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" umount /proc
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" umount /sys
+	sudo chroot "${REF_DISTRO_IMG_DIR_PATH}" umount /dev/pts
+
+	sudo umount "${REF_DISTRO_IMG_DIR_PATH}"/dev
+	sudo umount "${REF_DISTRO_IMG_DIR_PATH}"/run
 
 	return 0
 }
