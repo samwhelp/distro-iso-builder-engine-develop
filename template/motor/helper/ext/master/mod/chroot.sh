9e147e9cacb4c0c4397cd2a61456093a8782ffbf
samwhelp <samwhelp@users.noreply.github.com>
2025-05-27 16:41:42 +0800

    fix

--- template/motor/helper/ext/master/mod/chroot.sh
+++ template/motor/helper/ext/master/mod/chroot.sh
@@ -19,3 +19,28 @@ mod_chroot_session () {
 ##
 ### Tail: Master / Mod / Chroot
 ################################################################################
+
+
+################################################################################
+### Head: Master / Mod / Chroot / Mount
+##
+
+mod_distro_mount_for_chroot () {
+
+	sys_distro_unmount_for_chroot
+
+	sys_distro_mount_for_chroot
+
+	return 0
+}
+
+mod_distro_unmount_for_chroot () {
+
+	sys_distro_unmount_for_chroot
+
+	return 0
+}
+
+##
+### Tail: Master / Mod / Chroot / Mount
+################################################################################
---
+++ template/motor/helper/mak/distro.mk
@@ -0,0 +1,11 @@
+
+
+distro-unmount:
+	@distro-unmount.sh
+.PHONY: distro-unmount
+
+
+distro-mount:
+	@distro-mount.sh
+.PHONY: distro-mount
+
