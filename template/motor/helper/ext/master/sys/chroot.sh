28105e4b8073565fb16f99ee2931dc9cf8221d93
samwhelp <samwhelp@users.noreply.github.com>
2025-05-27 11:39:05 +0800

    fix

--- template/motor/helper/ext/master/sys/chroot.sh
+++ template/motor/helper/ext/master/sys/chroot.sh
@@ -4,8 +4,13 @@
 ### Head: Master / Sys / Chroot
 ##
 
+sys_chroot_run () {
 
+	local img_dir_path="${REF_DISTRO_IMG_DIR_PATH}"
 
+	sudo env DEBIAN_FRONTEND=noninteractive chroot "${img_dir_path}" ${@}
+
+}
 
 ##
 ### Tail: Master / Sys / Chroot
